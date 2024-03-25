99bc4f48250f334aba5d5f290ce5da568f057cdc
Khoa Cao <20521462@gm.uit.edu.vn>
2024-03-25 12:26:32 +0700

    update: initial commit

---
+++ android/app/src/main/kotlin/com/example/expense_management_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expense_management_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import './screens/login_screens.dart';
+import './screens/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => LoginScreen(),
+      },
+    );
+  }
+}
